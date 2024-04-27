d22f5eedc5a1b528123fe7e18bdb5d774dec5eac
Ilyas Al Furqon <ilyiyas05@gmail.com>
2024-04-28 01:58:53 +0800

    menambahkan fitur baru pada page menu dan menambahkan image

--- pages/menu.php
+++ pages/menu.php
@@ -1,1 +1,54 @@
-<h1>Ini halaman menu</h1>
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/menuimg/continentalbreakfast.jpg" class="d-block w-100 " alt="Continental Breakfast" style="height: 75.2vh;">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Continental Breakfast</h5>
+        <p>Fasilitas Gym dengan peralatan paling lengkap dan tidak ada di hotel manapun.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/menuimg/soup&salad.jpg" class="d-block w-100" alt=" soup&salad">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Soup & Salad</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/menuimg/filetmignon.jpg" class="d-block w-100" alt="FiletMignon">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Filet Mignon with Mashed Potatoes</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/menuimg/wine.jpg" class="d-block w-100" alt="Wine">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Wine and MORE</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/menuimg/frenchtoast.jpg" class="d-block w-100" alt="French Toast">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>French Toast</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
