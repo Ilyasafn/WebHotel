074c47617c5d4529d9316b48fd9444dc5723fe56
Ilyas Al Furqon <ilyiyas05@gmail.com>
2024-04-27 19:52:01 +0800

    Mengubah beberapa komponen home

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,73 @@
-<h1>Ini Page Home</h1>
+   <section class="row">
+        <div class="col-8">
+
+            <!-- ABOUT HOTEL -->
+            <p>LeafIn Hotel Balikpapan is a luxurious property conveniently located near the centre of Balikpapan City.</p>
+            <p>
+            <b>Facilities</b>
+            <br>
+            Dining facilities at Grand Jatra Hotel Pekanbaru include a cafeteria. There is also room service available.
+            <br>
+            <b>Leisure amenities :</b> 
+            There is an outdoor swimming pool at the hotel. Guests can also enjoy the following spa/wellness facilities: sauna, full-seravice health spa. Other services: computer rental, concierge services, dry cleaning/laundry service, medical assistance available, tours/ticket assistance and wedding services.</p>
+            <b>Transportation/Parking facilities</b>
+            <br>
+            <p>LeafIn Hotel Balikpapan offers transportation to/from the airport. Limo/Town car services can be arranged.</p>
+            <!-- ABOUT HOTEL END -->
+
+            <hr>
+
+            <!-- FASILITAS HOTEL -->
+            <h3>LeafIn Hotel Balikpapan Services and Facilities</h3>
+            <h6>Hotel Facilities</h6>
+            
+            <!-- LIST FASILITAS -->
+            <div class="row ms-1">
+                <ul class="col">
+                    <li>24 hour front desk</li>
+                    <li>ATM Machine</li>
+                    <li>Babysitting Services</li>
+                    <li>Balcony</li>
+                    <li>BBQ Grills</li>
+                    <li>Car Rental</li>
+                    <li>Coffee Maker</li>
+                    <li>Concierge</li>
+                    <li>Fitness Center</li>
+                    <li>Free Continental Breakfast</li>
+                </ul>
+                <ul class="col">
+                    <li>Hot Tub or Spa</li>
+                    <li>Lounge/Bar</li>
+                    <li>Mini-Bar</li>
+                    <li>Non-Smoking Facility</li>
+                    <li>Restaurant</li>
+                    <li>Room Service</li>
+                    <li>Swimming Pool - Indoor</li>
+                    <li>Swimming Pool - Outdoor</li>
+                    <li>Refrigerator</li>
+                    <li>Wheelchair Access</li>
+                </ul>
+                <ul class="col">
+                    <li>Contactless check-in and check-out are available</li>
+                    <li>Hair Dryer</li>
+                    
+                </ul>
+            </div>
+            <!-- LIST FASILITAS END -->
+            <!-- FASILITAS HOTEL END -->
+            <hr>
+            <!-- CARA BOOKING -->
+            <h3>How To Book LeafIn Hotel Balikpapan</h3>
+            <p>Making reservations for Grand Jatra Pekanbaru Hotel on this website is a secure and straightforward process. To check the nightly rates of Grand Jatra Pekanbaru Hotel, simply begin by selecting the desired dates/length of stay and click the "Check Rates" button. Comparison and decisions can then be made with the display of pricing information for each style of guest room. Information regarding room features and amenities is provided in the center of the screen. Having made the decision for dates, room style, and rates, simply locate and click on the "Book Now" button on the right hand side of the screen.</p>
+            <p>After making this selection you will be transferred to a "Complete Your Booking" form where various information will be requested in order to reserve your room at Grand Jatra Pekanbaru Hotel. The "Details of Your Stay" will be displayed for your review so that you can ensure all information regarding your visit is acceptable and understood. After reviewing the details, proceed by filling in the information requested in the "Guest Details" section. Answer the few basic questions required. Please be sure to look over all supplied answers so that important details such as name and email address are spelled correctly. Upon completing this, simply select the "Complete Reservation" button to finish booking your room at Grand Jatra Pekanbaru Hotel. The use of a secure commerce server ensures that all personal data such as credit card information is secure and encoded into a non-usable form.</p>
+            <!-- CARA BOOKING END -->
+
+        </div>
+
+
+        <div class="col">
+            <h2>Article</h2>
+        </div>
+
+
+    </section>
