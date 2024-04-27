658630f0a9ced396bff157bb10360d8fdc7ea205
Ilyas Al Furqon <ilyiyas05@gmail.com>
2024-04-28 01:20:28 +0800

    menambahkan beberapa img baru untuk page fasilitas

--- pages/facilities.php
+++ pages/facilities.php
@@ -3,26 +3,42 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
   <div class="carousel-inner">
-    <div class="carousel-item active h-50">
-      <img src="assets/img/facilityimg/gym.jpeg" class="d-block w-100" alt="...">
+    <div class="carousel-item active">
+      <img src="assets/img/facilityimg/gym.jpg" class="d-block w-100 " alt="Gym" style="height: 75.2vh;">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5>Gym</h5>
+        <p>Fasilitas Gym dengan peralatan paling lengkap dan tidak ada di hotel manapun.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="assets/img/facilityimg/pool.jpg" class="d-block w-100" alt="Outdoor Pool">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
+        <h5>Kolam Renang Luar</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="assets/img/facilityimg/finediningrest.jpeg" class="d-block w-100" alt="Restaurant">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
+        <h5>Fine Dining Restaurant</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/facilityimg/aula.jpeg" class="d-block w-100" alt="Restaurant">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>BallRoom</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/facilityimg/spa.jpg" class="d-block w-100" alt="Restaurant">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Spa</h5>
         <p>Some representative placeholder content for the third slide.</p>
       </div>
     </div>
