0247a082baaf4fa90d588dfc938e77b4adee1816
Ilyas Al Furqon <ilyiyas05@gmail.com>
2024-04-30 16:46:10 +0800

    menambahkan isi konten pada halaman your_book.php untuk melakukan validasi schedule atau room yang sudah di booking

--- pages/your_book.php
+++ pages/your_book.php
@@ -1,1 +1,15 @@
-<h1>Halaman Your Book</h1>
+<div class="mt-3 mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Nama</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukkan nama anda">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukkan email anda">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">No. Handphone</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="08...">
+</div>
+
+
+<button style="background-color: #E8DFCA; color: #FCFFE0;" onclick="mySchedule()" class="btn">Cek Booking!</button>
